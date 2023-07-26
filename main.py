17eaf3a4b9583f4d0896725eea0f927b38dd0400
Gabriel <gaby.tzu@windowslive.com>
2023-07-26 11:21:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ from utils import ExtractMatches, EloRatings
 
 
 if __name__ == "__main__":
-    e = EloRatings(ExtractMatches(start_year=2019).get_matches())
-    
-    e.query_interface(home_team="FC U Craiova", away_team="Universitatea Cluj")
-    e.query_interface(home_team="Rapid București", away_team="Botoșani")
-    e.query_interface(home_team="Hermannstadt", away_team="UTA Arad")
-    e.query_interface(home_team="Oțelul Galați", away_team="FCSB")
-    e.query_interface(home_team="Farul Constanța", away_team="Poli Iași")
-    e.query_interface(home_team="CFR Cluj", away_team="CS U Craiova")
-    e.query_interface(home_team="Petrolul Ploiești", away_team="Voluntari")
-    e.query_interface(home_team="Dinamo", away_team="Sepsi")
+    matches = ExtractMatches(start_year=2019, extract_historic_data=True)
+    future_matches = matches.get_future_matches()
+    e = EloRatings(matches.get_matches())
 
-    # e.export_results()
+    for k, v in future_matches.items():
+        e.query_interface(home_team=k, away_team=v)
+
+    e.export_results()
     e.see_win_perc()
