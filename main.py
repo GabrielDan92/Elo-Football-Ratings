d5b6d7688d4e65201bc733c7166b7d84d8f0a90b
Gabriel <gaby.tzu@windowslive.com>
2023-07-26 12:10:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from utils import ExtractMatches, EloRatings
 
 
 if __name__ == "__main__":
-    matches = ExtractMatches(start_year=2019, extract_historic_data=True)
-    future_matches = matches.get_future_matches()
-    e = EloRatings(matches.get_matches())
+    matches = ExtractMatches(start_year=2019)
+    played_matches = matches.get_played_matches()
+    scheduled_matches = matches.get_scheduled_matches()
 
-    for k, v in future_matches.items():
+    e = EloRatings(played_matches)
+
+    for k, v in scheduled_matches.items():
         e.query_interface(home_team=k, away_team=v)
 
     e.export_results()
