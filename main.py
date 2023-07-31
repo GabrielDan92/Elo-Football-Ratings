b397a34cd32ccc7e56f5303a0a4d336d64701884
Gabriel <gaby.tzu@windowslive.com>
2023-07-31 18:13:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from utils import ExtractMatches, EloRatings
 
 
 if __name__ == "__main__":
-    matches = ExtractMatches(start_year=2019)
+    matches = ExtractMatches(start_year=2018)
     played_matches = matches.get_played_matches()
     scheduled_matches = matches.get_scheduled_matches()
 
-    e = EloRatings(played_matches)
+    e = EloRatings(matches=played_matches, confidence=0.6)
+
+    # e.query_interface("Petrolul Ploiești", "Voluntari")
 
     for k, v in scheduled_matches.items():
         e.query_interface(home_team=k, away_team=v)
 
-    e.export_results()
+    # e.export_results()
     e.see_win_perc()
