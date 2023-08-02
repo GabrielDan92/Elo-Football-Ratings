2115e808167782aded1102f18aca81f94e8b01df
Gabriel <gaby.tzu@windowslive.com>
2023-08-02 15:32:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from utils import ExtractMatches, EloRatings
 
 
 if __name__ == "__main__":
-    matches = ExtractMatches(start_year=2018)
-    played_matches = matches.get_played_matches()
-    scheduled_matches = matches.get_scheduled_matches()
+    competitions = {
+        "UK Premier League": ExtractMatches(start_year=2017, comp="UK-Premier-League"),
+        "Spain La Liga": ExtractMatches(start_year=2017, comp="Spain-La-Liga"),
+        # "Germany Bundesliga": ExtractMatches(start_year=2017, comp="DE-Bundesliga"),
+        "RO Liga 1": ExtractMatches(start_year=2018, comp="RO-Liga-1"),
+    }
 
-    e = EloRatings(matches=played_matches, confidence=0.6)
+    for name, competition in competitions.items():
+        played_matches = competition.get_played_matches()
+        scheduled_matches = competition.get_scheduled_matches()
 
-    # e.query_interface("Petrolul Ploiești", "Voluntari")
+        e = EloRatings(matches=played_matches, confidence=0.6)
 
-    for k, v in scheduled_matches.items():
-        e.query_interface(home_team=k, away_team=v)
+        for k, v in scheduled_matches.items():
+            try:
+                e.query_interface(home_team=k, away_team=v)
+            except:
+                continue
 
-    # e.export_results()
-    e.see_win_perc()
+        # e.export_results(competition_name=name)
+        e.see_win_perc(competition_name=name)
