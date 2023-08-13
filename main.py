4dcf47cb06b0a400fe7facbc4ab0d82a35d293f5
Gabriel <gaby.tzu@windowslive.com>
2023-08-13 20:26:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from extract_matches import ExtractMatches
 
+
 if __name__ == "__main__":
-    Romania = ExtractMatches(comp="RO-Liga-1", start_year=2018, confidence=0.6)
+    # Australia = ExtractMatches(comp="AUSTRL-League", start_year=2017, confidence=0.6) # not in unibet
+    # Greece = ExtractMatches(comp="GREEK-Superliga", start_year=2018, confidence=0.6) # not in Unibet
+    Romania = ExtractMatches(comp="RO-Liga-1", start_year=2019, confidence=0.6)
     Germany = ExtractMatches(comp="DE-Bundesliga", start_year=2017, confidence=0.6)
     Italy = ExtractMatches(comp="IT-Serie-A", start_year=2018, confidence=0.6)
     France = ExtractMatches(comp="FR-Ligue-1", start_year=2019, confidence=0.6)
-    UK = ExtractMatches(comp="UK-Premier-League", start_year=2017)
-    Spain = ExtractMatches(comp="SP-La-Liga", start_year=2019)
-    # Brazil = ExtractMatches(comp="BRZ-Serie-A", start_year=2019)
+    UK = ExtractMatches(comp="UK-Premier-League", start_year=2017, confidence=0.6)
+    Spain = ExtractMatches(comp="SP-La-Liga", start_year=2019, confidence=0.6)
+    Brazil = ExtractMatches(comp="BRZ-Serie-A", start_year=2019, confidence=0.57)
     Belgium = ExtractMatches(comp="BLG-Pro-League", start_year=2020, confidence=0.59)
-    Croatia = ExtractMatches(comp="CROAT-League", start_year=2018)
+    Croatia = ExtractMatches(comp="CROAT-League", start_year=2018, confidence=0.6)
     Czech = ExtractMatches(comp="CZECH-League", start_year=2020, confidence=0.55)
     Netherlands = ExtractMatches(comp="NETHRL-League", start_year=2020, confidence=0.55)
     Scotland = ExtractMatches(comp="SCOT-League", start_year=2019, confidence=0.55)
+    Austria = ExtractMatches(comp="AUSTR-Bundesliga", start_year=2018, confidence=0.57)
+    Denmark = ExtractMatches(comp="DENMRK-Superliga", start_year=2018, confidence=0.59)
+    Finland = ExtractMatches(comp="FINL-Veikkausliiga", start_year=2018, confidence=0.58)
+    Portugal = ExtractMatches(comp="PORT-Superliga", start_year=2019, confidence=0.6)
+    Poland = ExtractMatches(comp="POLND-Ekstraklasa", start_year=2018, confidence=0.59)
+    Norway = ExtractMatches(comp="NORW-Eliteserien", start_year=2017, confidence=0.55)
+    Sweden = ExtractMatches(comp="SWE-Allsvenskan", start_year=2018, confidence=0.58)
