5f8ef722e8463b6699ed64ba35f7344bcfba9d31
Gabriel <gaby.tzu@windowslive.com>
2023-08-13 17:44:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,50 @@
+MAP = {
+    "RO-Liga-1": {
+        "suffix": "Liga-I-Scores-and-Fixtures",
+        "comp_id": 47,
+    },
+    "UK-Premier-League": {
+        "suffix": "Premier-League-Scores-and-Fixtures",
+        "comp_id": 9,
+    },
+    "SP-La-Liga": {
+        "suffix": "La-Liga-Scores-and-Fixtures",
+        "comp_id": 12,
+    },
+    "DE-Bundesliga": {
+        "suffix": "Bundesliga-Scores-and-Fixtures",
+        "comp_id": 20,
+    },
+    "IT-Serie-A": {
+        "suffix": "Serie-A-Scores-and-Fixtures",
+        "comp_id": 11,
+    },
+    "FR-Ligue-1": {
+        "suffix": "Ligue-1-Scores-and-Fixtures",
+        "comp_id": 13,
+    },
+    "BLG-Pro-League": {
+        "suffix": "Belgian-Pro-League-Scores-and-Fixtures",
+        "comp_id": 37,
+    },
+    "BRZ-Serie-A": {
+        "suffix": "Serie-A-Scores-and-Fixtures",
+        "comp_id": 24,
+    },
+    "CROAT-League": {
+        "suffix": "Hrvatska-NL-Scores-and-Fixtures",
+        "comp_id": 63,
+    },
+    "CZECH-League": {
+        "suffix": "Czech-First-League-Scores-and-Fixtures",
+        "comp_id": 66,
+    },
+    "NETHRL-League": {
+        "suffix": "Eredivisie-Scores-and-Fixtures",
+        "comp_id": 23,
+    },
+    "SCOT-League": {
+        "suffix": "Scottish-Premiership-Scores-and-Fixtures",
+        "comp_id": 40,
+    },
+}
